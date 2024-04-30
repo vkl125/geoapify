df9083498fab00b179be7f0f0bb10ccd518eb496
vkl125 <vkl125@gmail.com>
2024-04-30 17:23:57 +0800

    WIP popup on click

--- src/app/components/my-map/my-map.component.ts
+++ src/app/components/my-map/my-map.component.ts
@@ -9,7 +9,6 @@ import 'mapbox-gl-leaflet';
   styleUrls: ['./my-map.component.scss']
 })
 export class MyMapComponent implements OnInit, AfterViewInit {
-
   
   @ViewChild('map')
   private mapContainer: ElementRef<HTMLElement>;
@@ -27,6 +26,7 @@ export class MyMapComponent implements OnInit, AfterViewInit {
       position: { lat: 48.096980, lon: 11.555466 },
       limit: 100,
     };
+    
   }
 
   ngOnInit() {
@@ -36,14 +36,28 @@ export class MyMapComponent implements OnInit, AfterViewInit {
     this.myAPIKey = "0c04a07dfc3e449c9b0d3967063aa889"; //should be stored in config file
     const mapStyle = "https://maps.geoapify.com/v1/styles/osm-carto/style.json";
     
-     const map = L.map('my-map').setView([48.096980, 11.555466], 15);//22.335594, 114.160622
-    // const map = new L.Map(this.mapContainer.nativeElement).setView(
-    //   [initialState.lat, initialState.lng],
-    //   initialState.zoom
+    //probably want to save/fetch where user is based on last session.
+     this.map = L.map('my-map').setView([48.096980, 11.555466], 15);//22.335594, 114.160622
     // );
-
+    this.map.on('click', (e) => { 
+      //have context menu showing ? or just create marker?
+      var formpopup = L.popup();
+      const markerIcon = L.icon({
+        iconUrl: `https://api.geoapify.com/v1/icon?size=xx-large&type=awesome&color=%233e9cfe&icon=paw&apiKey=${this.myAPIKey}`,
+        iconSize: [31, 46], // size of the icon
+        iconAnchor: [e.latlng.lat, e.latlng.lng], // point of the icon which will correspond to marker's location
+        popupAnchor: [0, -45] // point from which the popup should open relative to the iconAnchor
+      });
+      //need to modify popup so that we can delete this marker
+      const zooMarkerPopup = L.popup().setContent(`"This is set at ${e.latlng.lat}, ${e.latlng.lng}`);
+      const zooMarker = L.marker([e.latlng.lat, e.latlng.lng], {//22.335594, 114.160622
+        icon: markerIcon
+      }).bindPopup(zooMarkerPopup).addTo(this.map);
+      //setup API call to save this coordinate marker
+        
+    });
     // the attribution is required for the Geoapify Free tariff plan
-    map.attributionControl
+    this.map.attributionControl
       .setPrefix("")
       .addAttribution(
         'Powered by <a href="https://www.geoapify.com/" target="_blank">Geoapify</a> | © OpenStreetMap <a href="https://www.openstreetmap.org/copyright" target="_blank">contributors</a>'
@@ -52,7 +66,7 @@ export class MyMapComponent implements OnInit, AfterViewInit {
     L.mapboxGL({
       style: `${mapStyle}?apiKey=${this.myAPIKey}`,
       accessToken: "no-token"
-    }).addTo(map);
+    }).addTo(this.map);
 
     const markerIcon = L.icon({
       iconUrl: `https://api.geoapify.com/v1/icon?size=xx-large&type=awesome&color=%233e9cfe&icon=paw&apiKey=${this.myAPIKey}`,
@@ -64,14 +78,32 @@ export class MyMapComponent implements OnInit, AfterViewInit {
     const zooMarkerPopup = L.popup().setContent("This is Munich Zoo");
     const zooMarker = L.marker([48.096980, 11.555466], {//22.335594, 114.160622
       icon: markerIcon
-    }).bindPopup(zooMarkerPopup).addTo(map);
+    }).bindPopup(zooMarkerPopup).addTo(this.map);
 
+  }
+  //map click
+  onEachFeature = (feature, layer) => {
+    //bind click
+    layer.on({
+        click: this.onMapClick(feature)
+    });
+  }
+  onMapClick = async (event) => {
+    var popup = L.popup()
+    .setLatLng(event)
+    .setContent('<p>Hello world!<br />This is a nice popup.</p>')
+    .openOn(this.map);
   }
   //current location
   currentLocation = async () => {
-    const response = await fetch(`https://api.geoapify.com/v1/ipinfo?apiKey=${this.myAPIKey}`);
-    const data = await response.json();      
-
+    const response = await fetch(`https://api.geoapify.com/v1/ipinfo?ip=61.93.166.37&apiKey=${this.myAPIKey}`);
+    const data = await response.json();     
+    console.log(data); 
+    this.setCoord(data.location.latitude, data.location.longitude);
+  }
+  //set coordinates
+  setCoord = async (lat, lng) => {
+    this.map.setView(new L.LatLng(lat, lng));
   }
   onSuggestionsChanged(suggestions: GeoJSON.Feature[]): void {
     // Handle the updated suggestions list
@@ -80,5 +112,6 @@ export class MyMapComponent implements OnInit, AfterViewInit {
   onPlaceSelected(place: any): void {
     // Handle the selected place, which is a GeoJSON feature
     console.log('Selected Place:', place);
+    this.setCoord(place.geometry.coordinates[1], place.geometry.coordinates[0])
   }
 }
